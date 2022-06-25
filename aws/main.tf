52e90535db68654d57a943eebd1a08707cc0c592
David Crotty <david.crotty@playsportsnetwork.com>
2022-06-25 17:32:28 +0100

    adds back extra vars

--- aws/main.tf
+++ aws/main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "instance" {
       "sudo apt update",
       "sudo apt install ansible git -y",
       "git clone https://github.com/davidcrotty/minecraft.git /tmp/minecraft",
-      "ansible-playbook /tmp/minecraft/playbooks/minecraft_aws.yml"
+      "ansible-playbook /tmp/minecraft/playbooks/minecraft_aws.yml --extra-vars \"target=localhost mc_memory=7168\""
     ]
   }
 
