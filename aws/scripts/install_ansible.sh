6ee50a9b8b65379da0a04dfb0e466c689be07a38
David Crotty <david.crotty@playsportsnetwork.com>
2022-06-25 20:35:35 +0100

    memory

--- aws/scripts/install_ansible.sh
+++ aws/scripts/install_ansible.sh
@@ -7,4 +7,4 @@ done
 apt-get update
 sudo apt install ansible git -y
 git clone https://github.com/davidcrotty/minecraft.git /tmp/minecraft
-ansible-playbook /tmp/minecraft/playbooks/minecraft_aws.yml --extra-vars "target=localhost mc_memory=7168"
+ansible-playbook /tmp/minecraft/playbooks/minecraft_aws.yml --extra-vars "target=localhost mc_memory=6000"
