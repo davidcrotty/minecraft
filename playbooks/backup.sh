e8e3cc382e777fc67b6b40cfa39dd173f7dc0ccc
David Crotty <david.crotty.work@gmail.com>
2022-07-05 18:18:19 +0100

    updates backup script

--- playbooks/backup.sh
+++ playbooks/backup.sh
@@ -7,4 +7,12 @@ BACKUP_NAME=server_backup_$(date +"%F_%H_%M_%S").tar.gz
 
 tar cvf - /opt/minecraft/server | gzip -9 > $BACKUP_DIR/$BACKUP_NAME
 
+echo "Backup up to aws"
+
 aws s3 cp $BACKUP_DIR$BACKUP_NAME s3://minecraft-backups-ea33578e-183a-4888-8716-8c001c7144c9
+
+TO_REMOVE="$BACKUP_DIR/$BACKUP_NAME.tar.gz"
+
+echo "removing file $TO_REMOVE"
+
+rm $TO_REMOVE
