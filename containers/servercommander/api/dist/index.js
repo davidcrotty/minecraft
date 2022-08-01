42a79c00a1433893d8dc58b28be703282fabfca5
David Crotty <david.crotty@playsportsnetwork.com>
2022-08-01 21:56:05 +0100

    adds logging

--- containers/servercommander/api/dist/index.js
+++ containers/servercommander/api/dist/index.js
@@ -4,10 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const winston_1 = require("winston");
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+const logger = (0, winston_1.createLogger)({
+    transports: [new winston_1.transports.Console()],
+    format: winston_1.format.combine(winston_1.format.colorize(), winston_1.format.timestamp(), winston_1.format.printf(({ timestamp, level, message }) => {
+        return `[${timestamp}] ${level}: ${message}`;
+    })),
+});
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
+});
+app.post('/minecraft', (req, res) => {
+    logger.info("Starting server");
+    res.status(201).end();
 });
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
--- containers/servercommander/api/index.ts
+++ containers/servercommander/api/index.ts
@@ -1,11 +1,27 @@
 import express, { Express, Request, Response } from 'express';
+import { createLogger, transports, format } from "winston";
 
 
 const app: Express = express();
 const port = process.env.PORT;
+const logger = createLogger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level}: ${message}`;
+    })
+  ),
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/minecraft', (req: Request, res: Response) => {
+  logger.info("Starting server minecraft server");
+  res.status(201).end();
 });
 
 app.listen(port, () => {
