0027d6d7444b4bd3ca8428bea9d05ff8feb82aab
David Crotty <david.crotty@playsportsnetwork.com>
2022-08-01 23:17:18 +0100

    wip lambda

--- containers/servercommander/api/dist/index.js
+++ containers/servercommander/api/dist/index.js
@@ -1,25 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const winston_1 = require("winston");
-const app = (0, express_1.default)();
-const port = process.env.PORT;
 const logger = (0, winston_1.createLogger)({
     transports: [new winston_1.transports.Console()],
     format: winston_1.format.combine(winston_1.format.colorize(), winston_1.format.timestamp(), winston_1.format.printf(({ timestamp, level, message }) => {
         return `[${timestamp}] ${level}: ${message}`;
     })),
 });
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
-});
-app.post('/minecraft', (req, res) => {
-    logger.info("Starting server");
-    res.status(201).end();
-});
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+console.log("script has been invoked - turning on");
--- containers/servercommander/api/index.ts
+++ containers/servercommander/api/index.ts
@@ -1,9 +1,5 @@
-import express, { Express, Request, Response } from 'express';
 import { createLogger, transports, format } from "winston";
 
-
-const app: Express = express();
-const port = process.env.PORT;
 const logger = createLogger({
   transports: [new transports.Console()],
   format: format.combine(
@@ -15,15 +11,4 @@ const logger = createLogger({
   ),
 });
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.post('/minecraft', (req: Request, res: Response) => {
-  logger.info("Starting server minecraft server");
-  res.status(201).end();
-});
-
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+console.log("script has been invoked - turning on");
