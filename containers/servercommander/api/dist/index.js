099e548bd5d104b6a42463ef01f61ccb31367901
David Crotty <david.crotty@playsportsnetwork.com>
2022-08-03 23:00:34 +0100

    terraform apply wip

--- containers/servercommander/api/dist/index.js
+++ containers/servercommander/api/dist/index.js
@@ -8,14 +8,30 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const child_process_1 = require("child_process");
+const stream_to_string_1 = __importDefault(require("stream-to-string"));
 const handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Running terraform plan");
     let terraformInit = yield (0, child_process_1.exec)(`terraform init`);
     if (terraformInit.stdout) {
         console.log("success");
+        let result = yield (0, stream_to_string_1.default)(terraformInit.stdout);
+        console.log(result);
+        console.log("applying terraform config");
+        let terraformApply = yield (0, child_process_1.exec)(`terraform apply -var-file="secret.tfvars" main.tf -auto-approve`);
+        if (terraformApply.stdout) {
+            console.log("terraform apply success");
+            let result = yield (0, stream_to_string_1.default)(terraformApply.stdout);
+            console.log(result);
+        }
+        else if (terraformApply.stderr) {
+            console.log("terraform apply error");
+        }
     }
     else if (terraformInit.stderr) {
         console.log("error");
--- containers/servercommander/api/index.ts
+++ containers/servercommander/api/index.ts
@@ -1,5 +1,6 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 import { exec } from 'child_process';
+import streamToString from 'stream-to-string';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   console.log("Running terraform plan");
@@ -7,7 +8,21 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
   let terraformInit = await exec(`terraform init`);
 
   if (terraformInit.stdout) {
-    console.log("success")
+    console.log("success");
+    let result = await streamToString(terraformInit.stdout);
+    console.log(result);
+
+    console.log("applying terraform config");
+    let terraformApply = await exec(`terraform apply -var-file="secret.tfvars" main.tf -auto-approve`);
+
+    if(terraformApply.stdout) {
+      console.log("terraform apply success");
+      let result = await streamToString(terraformApply.stdout);
+      console.log(result);
+    } else if (terraformApply.stderr) {
+      console.log("terraform apply error");
+    }
+
   } else if (terraformInit.stderr) {
     console.log("error")
   } else {
